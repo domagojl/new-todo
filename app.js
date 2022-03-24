6341ca79bcb7a80e1a29fddf730d4ce46166a1fc
Domagoj <domag14@gmail.com>
2022-03-24 19:21:10 +0100

    init

---
+++ app.js
@@ -0,0 +1,69 @@
+//selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+
+
+//event listeners
+
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click' , deleteCheck);
+
+
+
+
+//functions
+
+function addTodo(event){
+    event.preventDefault();
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newTodo);
+
+    //completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>'
+    completedButton.classList.add("complete-btn")
+    todoDiv.appendChild(completedButton)
+
+    //trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class = "fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn")
+    todoDiv.appendChild(trashButton)
+
+
+    todoList.appendChild(todoDiv);
+
+    //clear todo inout value
+
+    todoInput.value = "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+
+    //delete todo
+
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.classList.add("fall")
+        todo.addEventListener('transitioned', function(){
+            todo.remove()
+        });
+    }
+
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
